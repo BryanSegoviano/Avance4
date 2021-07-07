4d8f3aaa542bbb314622f79bfabb98398cef8737
BryanS <bryanisaac77@hotmail.com>
2021-07-06 18:36:16 -0700

    Proyecto Final Avance 4

--- dominio/target/generated-sources/annotations/dominio/Comentario_.java
+++ dominio/target/generated-sources/annotations/dominio/Comentario_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-07-06T18:15:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-07-06T18:32:07")
 @StaticMetamodel(Comentario.class)
 public class Comentario_ { 
 
